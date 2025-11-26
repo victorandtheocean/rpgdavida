97c4e700005b1096cccf593c82dca0cbe6577ce3
victorandtheocean <contact@victorandtheocean.com>
2025-11-26 07:10:20 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+{
+  "name": "RPG da Vida - Deep Ocean",
+  "short_name": "RPG Vida",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#001e36",
+  "theme_color": "#00e5ff",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "./icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "./icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
